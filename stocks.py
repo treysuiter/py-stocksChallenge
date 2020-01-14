612ebdc00586d210514db36a63dd0fc91620b762
Trey Suiter <trey.suiter@gmail.com>
2020-01-14 14:50:45 -0600

    first part of challenge complete

---
+++ stocks.py
@@ -0,0 +1,22 @@
+stockDict = {
+    "GE": "General Motors",
+    "CAT":"Caterpillar",
+    "EK":"Eastman Kodak"
+}
+
+purchases = [
+    ( 'GE', 100, '10-sep-2001', 48 ),
+    ( 'CAT', 100, '1-apr-1999', 24 ),
+    ( 'GE', 200, '1-jul-1998', 56 )
+]
+
+# for item in purchases:
+#     print(item[0], item[1], item[2])
+
+# for stock, name in stockDict.items():
+#     print(stock, name)
+
+for purchase in purchases:
+    print(f"{stockDict['GM']}")
+    print(purchase[0])
+    print(f"I purchased {stockDict[purchase[0]]}")
