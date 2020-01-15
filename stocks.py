9701364085041a9453ec58b8977a042032e513b4
Trey Suiter <trey.suiter@gmail.com>
2020-01-14 20:23:09 -0600

    report working; need to write print statement

--- stocks.py
+++ stocks.py
@@ -1,13 +1,13 @@
 stockDict = {
     "GE": "General Motors",
-    "CAT":"Caterpillar",
-    "EK":"Eastman Kodak"
+    "CAT": "Caterpillar",
+    "EK": "Eastman Kodak"
 }
 
 purchases = [
-    ( 'GE', 100, '10-sep-2001', 48 ),
-    ( 'CAT', 100, '1-apr-1999', 24 ),
-    ( 'GE', 200, '1-jul-1998', 56 )
+    ('GE', 100, '10-sep-2001', 48),
+    ('CAT', 100, '1-apr-1999', 24),
+    ('GE', 200, '1-jul-1998', 56)
 ]
 
 # for item in purchases:
@@ -16,6 +16,33 @@ purchases = [
 # for stock, name in stockDict.items():
 #     print(stock, name)
 
+# for purchase in purchases:
+
+#     print(f"I purchased {stockDict[purchase[0]]} stock for ${purchase[1] * purchase[3]}.")
+
+
+report = {}
+
 for purchase in purchases:
-    
-    print(f"I purchased {stockDict[purchase[0]]} stock for ${purchase[1] * purchase[3]}.")
+
+    # print(purchase[0])
+
+    if purchase[0] in report:
+
+        print('symbol', purchase[0])
+        report[purchase[0]] = report[purchase[0]].append(purchase)
+
+    else:
+        print(purchase, 'this is new purchase')
+        # report[purchase[0]] = 
+        report[purchase[0]] = [purchase]
+
+
+print('report', report)
+
+# test_dict = {}
+
+# test_dict["entry_one"] = []
+# test_dict["entry_one"].append("This")
+
+# print(test_dict)
