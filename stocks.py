eaf8770c45830d5280bde8f0277cb24a5b11548d
Trey Suiter <trey.suiter@gmail.com>
2020-01-15 06:58:39 -0600

    changed tup indexes to vars

--- stocks.py
+++ stocks.py
@@ -24,12 +24,14 @@ purchases = [
 report = {}
 
 for purchase in purchases:
+ 
+    stock_symbol = purchase[0] 
 
     if purchase[0] in report:
-        report[purchase[0]].append(purchase)
+        report[stock_symbol].append(purchase)
 
     else:
-        report[purchase[0]] = [purchase]
+        report[stock_symbol] = [purchase]
 
 for report_section in report.items():
 
@@ -37,9 +39,13 @@ for report_section in report.items():
 
     print(f"------ {report_section[0]} ------")
     for report_entry in report_section[1]:
-        # print(report_entry, "report entry")
-        print(f"{report_entry[1]} shares at {report_entry[3]} dollars on {report_entry[2]}")
-        total += report_entry[1] * report_entry[3]
+        #Set variables for indexes of tup
+        num_shares = report_entry[1]
+        purchase_price = report_entry[3]
+        purchase_date = report_entry[2]
+
+        print(f"{num_shares} shares at {purchase_price} dollars on {purchase_date}")
+        total += num_shares * purchase_price
     print()
     print(f"Total value of stock in portfolio: ${total}.")
     print()
